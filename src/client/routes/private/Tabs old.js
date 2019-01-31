256372300eb6e6d67503386ef732288cb3e449ea
Kiril Stas <vesground@gmail.com>
2019-01-31 14:19:34 +0200

    Added tab controller in private pages

---
+++ src/client/routes/private/Tabs old.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import { View, Text, Animated, TouchableOpacity, StyleSheet } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+// import { Constants } from 'expo';
+
+const FirstRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+);
+
+export default class TabViewExample extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderTabBar = props => {
+    const inputRange = props.navigationState.routes.map((x, i) => i);
+
+    return (
+      <View style={styles.tabBar}>
+        {props.navigationState.routes.map((route, i) => {
+          const color = props.position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(
+              inputIndex => (inputIndex === i ? '#D6356C' : '#222')
+            ),
+          });
+          return (
+            <TouchableOpacity
+              style={styles.tabItem}
+              onPress={() => this.setState({ index: i })}>
+              <Animated.Text style={{ color }}>{route.title}</Animated.Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  };
+
+  _renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+  });
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderTabBar={this._renderTabBar}
+        onIndexChange={this._handleIndexChange}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabBar: {
+    flexDirection: 'row',
+    paddingTop: 0,
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 16,
+  },
+});
